7b4cb11e58011a19cda1eb49e8d5597ce825af92
Denise Ichinco <ichinco@gmail.com>
2014-08-03 12:43:24 -0400

    created rails project

---
+++ dinnerafterwork/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dinnerafterwork::Application.config.secret_key_base = '1e03e0266764518f419a040d66a3d394328b3f81649484c926eeef9e4aa7d41ef21bfdca07cc20b8cdece53787038e4226f90bb483a9471df4eae139ccc05082'
---
+++ dinnerafterwork/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dinnerafterwork::Application.config.session_store :cookie_store, key: '_dinnerafterwork_session'
